167c1ec9a0309c13e0452ba67b0789352730cbf3
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-23 11:57:09 +0200

    p7 - Improve isPrime()

--- p7_nthPrimeNumber.js
+++ p7_nthPrimeNumber.js
@@ -14,11 +14,9 @@ const nthPrime = (n) => {
 };
 
 const isPrime = (n) => {
-	if (n === 1 || n === 2) return true;
-
-	for (let i = 3; i <= Math.sqrt(n); i += 2) {
+	for (let i = 2; i <= Math.sqrt(n); i++) {
 		if (n % i === 0) return false;
 	}
 
-	return true;
+	return n > 1;
 };
