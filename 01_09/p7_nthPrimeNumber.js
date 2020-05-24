b35c23c743c473cc6fc6eb8a2ce3ea4b00e41954
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-24 09:15:17 +0200

    p7 - Improve algo & readability

--- 01_09/p7_nthPrimeNumber.js
+++ 01_09/p7_nthPrimeNumber.js
@@ -1,5 +1,5 @@
 const nthPrime = (n) => {
-	let count = 2;
+	let count = 2; // [2, 3]
 	let numberTest = 3;
 
 	while (count < n) {
@@ -8,13 +8,14 @@ const nthPrime = (n) => {
 			count++;
 		}
 	}
-
-	console.log(n, numberTest);
 	return numberTest;
 };
 
 const isPrime = (n) => {
-	for (let i = 2; i <= Math.sqrt(n); i++) {
+	// Even number cannot be prime except 2
+	if (n % 2 === 0) return false;
+
+	for (let i = 3; i <= Math.sqrt(n); i += 2) {
 		if (n % i === 0) return false;
 	}
 
