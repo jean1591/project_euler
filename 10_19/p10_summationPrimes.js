521c1138a33f650d39e03c86195b64a57eab8904
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-24 09:57:13 +0200

    p10 - Refacto

--- 10_19/p10_summationPrimes.js
+++ 10_19/p10_summationPrimes.js
@@ -1,5 +1,5 @@
 const primeSummation = (n) => {
-	let result = 0;
+	let result = 2;
 
 	for (let i = 1; i < n; i++) {
 		if (isPrime(i)) {
@@ -11,6 +11,7 @@ const primeSummation = (n) => {
 };
 
 const isPrime = (num) => {
-	for (let i = 2; i <= Math.sqrt(num); i++) if (num % i === 0) return false;
+	if (num % 2 === 0) return false;
+	for (let i = 3; i <= Math.sqrt(num); i += 2) if (num % i === 0) return false;
 	return num > 1;
 };
